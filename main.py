ea5a49e2010d8f7d3f3d397a32530e6729de429b
aadhil72 <aadhilj935@gmail.com>
2026-09-17 19:25:55 +0530

    Validate uploaded room images

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI,UploadFile,File,HTTPException
+from PIL import Image,UnidentifiedImageError
+from io import BytesIO
 
 app = FastAPI()
 
@@ -10,8 +12,19 @@ def health_check():
 async def analyze_room(image:UploadFile=File(...)):
 
     if image.content_type not in ["image/jpeg","image/png"]:
-        raise HTTPException(status_code=400,detail="Only jpeg,jpg and png images are allowed")
+        raise HTTPException(status_code=400,detail="Only jpeg and png images are allowed")
+
+    image_data = await image.read()
+    try:
+        room_image = Image.open(BytesIO(image_data))
+        room_image.load()
+    except UnidentifiedImageError:
+        raise HTTPException(status_code=400,detail="The uploaded file is not a valid image")
+
     return {
         "filename":image.filename,
-        "content_type":image.content_type
+        "content_type":image.content_type,
+        "format":room_image.format,
+        "size":room_image.size,
+        "mode":room_image.mode
         }
